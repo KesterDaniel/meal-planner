d13d2f5506feeb90d8f9f515da99ecffbb34b49d
KesterDaniel <kesterdan17@gmail.com>
2022-11-01 10:25:49 +0100

    created a food reducer to handle food state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -63,4 +63,18 @@ function calendar(state=initialCalenderState, action){
     }
 }
 
+function food(state={}, action){
+    switch (action.type) {
+        case ADD_RECIPE:
+            const {recipe} = action
+
+            return {
+                ...state,
+                [recipe.label]: recipe
+            }
+        default:
+            return state
+    }
+}
+
 export default calendar
